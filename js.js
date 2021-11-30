8f0f9fb057e4daa149aec95b1c1741e7a863204e
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-30 18:14:25 +0330

    arr-login-admin

---
+++ js.js
@@ -0,0 +1,8 @@
+var data = [
+    {id : 1 , name : "hasssan" , family : "hasani" , age : 21},
+    {id : 2 , name : "ali" , family : "hasani" , age : 19},
+    {id : 3 , name : "amir" , family : "jalali" , age : 22},
+]
+data.forEach(function(userData) {
+        console.log(`name: ${userData.name}  family: ${userData.family}`)
+})
